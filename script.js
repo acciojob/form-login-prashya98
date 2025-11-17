b98c71686ceb201adf4aaa03ef6a832517d46626
Prashant Kumar Ray <159636396+prashya98@users.noreply.github.com>
2025-11-17 19:39:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+    event.preventDefault();
+
+    // Get form fields
+    const firstName = document.forms["form1"]["fname"].value.trim();
+    const lastName = document.forms["form1"]["lname"].value.trim();
+
+    // Show alert with full name
+    alert(firstName + " " + lastName);
 
 }
